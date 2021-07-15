5b97dedaecd82e7b0f4a0eade8ccd21eab44e2ae
Sergey <thefanthomasfan@gmail.com>
2021-07-15 18:14:11 +0200

    Cleaned up some code

--- views/JavaScript/combinations.js
+++ views/JavaScript/combinations.js
@@ -2,13 +2,13 @@
 function singles(element) {
     for (let i = 0; i < dievalues.length; i++) {
         if (dievalues[i] == element) {
-            totalsum += dievalues[i];
-            bonus += dievalues[i];
+            totalsum += dievalues[i]
+            bonus += dievalues[i]
         }
     }
 
-    finished[0]++;
-    disable(element-1);
+    finished[0]++
+    disable(element-1)
 }
 
 
@@ -18,25 +18,25 @@ function pairs(nr, native) {
     pairvalues.push(pairsHelp(dievalues, 0, 0))
     pairvalues.push(pairsHelp(dievalues, pairvalues[0], 0))
     pairvalues.push(pairsHelp(dievalues, pairvalues[0], pairvalues[1]))
-    pairvalues.sort((a, b) => b - a);
+    pairvalues.sort((a, b) => b - a)
 
     if (native) {
         if (nr == 6 && pairvalues[0] != 0) {
-            totalsum += pairvalues[0];
+            totalsum += pairvalues[0]
         }
         else if (nr == 7 && pairvalues[0] != 0 && pairvalues[1] != 0) {
-            totalsum += pairvalues[0] + pairvalues[1];
+            totalsum += pairvalues[0] + pairvalues[1]
         }
         else if (nr == 8 && pairvalues[0] != 0 && pairvalues[1] != 0 && pairvalues[2] != 0) {
-            totalsum += pairvalues.reduce((a, b) => a + b, 0);
+            totalsum += pairvalues.reduce((a, b) => a + b, 0)
         }
 
-        finished[1]++;
-        disable(nr);
-        return;
+        finished[1]++
+        disable(nr)
+        return
     }
 
-    return pairvalues;
+    return pairvalues
 }
 
 
@@ -45,24 +45,24 @@ function pairsHelp(arr, ex1, ex2) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i+1; j < arr.length; j++) {
             if (arr[i] == arr[j] && (arr[i] * 2 != ex1) && (arr[i] * 2 != ex2)) {
-                return arr[i] * 2;
+                return arr[i] * 2
             }
         }
     }
 
-    return 0;
+    return 0
 }
 
 
 /* Kinds  -  Keeps a hashmap of values and their occurences */
 function kinds(nr, native, tower) {
-    let map = new Map();
-    let tbr = [];
+    let map = new Map()
+    let tbr = []
 
     for (let i = 0; i < dievalues.length; i++) {
         for (let j = i+1; j < dievalues.length; j++) {
             if (dievalues[i] == dievalues[j]) {
-                map.set(dievalues[i], map.get(dievalues[i]) == undefined ? 1 : map.get(dievalues[i]) + 1);
+                map.set(dievalues[i], map.get(dievalues[i]) == undefined ? 1 : map.get(dievalues[i]) + 1)
             }
         }
     }
@@ -73,33 +73,33 @@ function kinds(nr, native, tower) {
                 totalsum += value * 5
             }
             else if (nr == 10 && key >= 6) {
-                totalsum += value * 4;
+                totalsum += value * 4
             }
             else if (nr == 9 && key >= 3) {
-                totalsum += value * 3;
+                totalsum += value * 3
             }
         })
 
-        finished[2]++;
-        disable(nr);
-        return;
+        finished[2]++
+        disable(nr)
+        return
     }
 
     if (tower) {
         map.forEach((key, value) => {
             if (key == 6) {
-                tbr.push(value);
+                tbr.push(value)
             }
         })
     } else {
         map.forEach((key, value) => {
             if (key >= 3) {
-                tbr.push(value);
+                tbr.push(value)
             }
         })
     }
 
-    return tbr.length == 2 ? tbr.sort((a, b) => b - a) : tbr;
+    return tbr.sort((a, b) => b - a)
 }
 
 
@@ -107,91 +107,90 @@ function kinds(nr, native, tower) {
 function straights(arr, nr) {
     for (let i = 0; i < arr.length; i++) {
         if (!dievalues.includes(arr[i])) {
-            finished[3]++;
-            
-            disable(nr);
-            return;
+            finished[3]++
+            disable(nr)
+            return
         }
     }
 
-    totalsum += arr.reduce((a, b) => a + b, 0);
-    finished[3]++;
-    disable(nr);
+    totalsum += arr.reduce((a, b) => a + b, 0)
+    finished[3]++
+    disable(nr)
 }
 
 
 /* Cottage  -  Combines the biggest 3kind with a pair */
 function cottage() {
-    let kind = kinds(0, false, false);
-    let pair = pairs(0, false);
+    let kind = kinds(0, false, false)
+    let pair = pairs(0, false)
 
     if (kind.length > 0 && pair.length > 0) {
-        totalsum += (kind[0] * 2) != pair[0] ? kind[0] * 3 + pair[0] : pair[1] != 0 ? kind[0] * 3 + pair[1] : 0;
+        totalsum += (kind[0] * 2) != pair[0] ? kind[0] * 3 + pair[0] : pair[1] != 0 ? kind[0] * 3 + pair[1] : 0
     }
 
-    finished[4]++;
-    disable(15);
+    finished[4]++
+    disable(15)
 }
 
 
 /* House  -  Combines two 3kinds */
 function house() {
-    let kind = kinds(0, false, false);
-    totalsum += kind.length == 2 ? kind[0] * 3 + kind[1] * 3 : 0;
+    let kind = kinds(0, false, false)
+    totalsum += kind.length == 2 ? kind[0] * 3 + kind[1] * 3 : 0
 
-    finished[4]++;
-    disable(16);
+    finished[4]++
+    disable(16)
 }
 
 
 /* Tower  -  Combines a pair and a 4kind */
 function tower() {
-    let kind = kinds(0, false, true);
-    let pair = pairs(0, false);
+    let kind = kinds(0, false, true)
+    let pair = pairs(0, false)
 
     if (pair[0] != 0 && pair[1] != 0 && kind.length > 0) {
-        totalsum += (kind[0] * 2) != pair[0] ? kind[0] * 4 + pair[0] : kind[0] * 4 + pair[1];
+        totalsum += (kind[0] * 2) != pair[0] ? kind[0] * 4 + pair[0] : kind[0] * 4 + pair[1]
     }
 
-    finished[4]++;
+    finished[4]++
     disable(17)
 }
 
 
 /* Chance  -  Simply adds sum of all die */
 function chance() {
-    totalsum += dievalues.reduce((a, b) => a + b, 0);
-    finished[5]++;
-    disable(18);
+    totalsum += dievalues.reduce((a, b) => a + b, 0)
+    finished[5]++
+    disable(18)
 }
 
 
 /* Yatzy  -  If all die show the same digit, award 100 + sum of die */
 function yatzy() {
     if (dievalues[0] == 0) {
-        finished[5]++;
-        disable(19);
-        return;
+        finished[5]++
+        disable(19)
+        return
     }
 
     if (dievalues.every(val => val == dievalues[0] )) {
-        totalsum += 100 + dievalues.reduce((a, b) => a + b, 0);
+        totalsum += 100 + dievalues.reduce((a, b) => a + b, 0)
     }
 
-    finished[5]++;
-    disable(19);
+    finished[5]++
+    disable(19)
 }
 
 
 /* Straight flush  -  If die show an ordered big straight, award the jackpot! */
 function straightflush(arr) {
     if (JSON.stringify(dievalues) != JSON.stringify(arr)) {
-        finished[5]++;
-        disable(20);
-        return;
+        finished[5]++
+        disable(20)
+        return
     }
 
-    totalsum += 150;
-    finished[5]++;
-    disable(20);
+    totalsum += 150
+    finished[5]++
+    disable(20)
 }
--- views/JavaScript/main.js
+++ views/JavaScript/main.js
@@ -1,46 +1,46 @@
 /* Dice relevant */
-const diceroller = document.querySelector("#diceroller");
-const die = document.getElementsByClassName("die");
-const dievalues = [0, 0, 0, 0, 0, 0];
-const locked = [false, false, false, false, false, false];
+const diceroller = document.querySelector("#diceroller")
+const die = document.getElementsByClassName("die")
+const dievalues = [0, 0, 0, 0, 0, 0]
+const locked = [false, false, false, false, false, false]
 
 /* Categories relevant */
-const categories = document.getElementsByClassName("categories");
-const category = document.getElementsByClassName("category");
-const finished = [0, 0, 0, 0, 0, 0];
+const categories = document.getElementsByClassName("categories")
+const category = document.getElementsByClassName("category")
+const finished = [0, 0, 0, 0, 0, 0]
 
 /* Modal relevant */
-const modal = document.querySelector("#modal");
-const sumbutton = document.querySelector("#sumbutton");
+const modal = document.querySelector("#modal")
+const sumbutton = document.querySelector("#sumbutton")
 
 /* Die rolls */
-const dierollmsg = document.querySelector("#dieroll-msg");
-let remainingrolls = 3;
-let rolled = 0;
+const dierollmsg = document.querySelector("#dieroll-msg")
+let remainingrolls = 3
+let rolled = 0
 
 /* Total sum and bonus */
-let totalsum = 0;
-let bonus = 0;
+let totalsum = 0
+let bonus = 0
 
 
 /* Roll dice */
 function rolldice() {
     if (remainingrolls < 1) {
-        dierollmsg.innerHTML = "No rolls left.";
-        return;
+        dierollmsg.innerHTML = "No rolls left."
+        return
     }
 
-    rolled++;
-    remainingrolls--;
+    rolled++
+    remainingrolls--
     dierollmsg.innerHTML = remainingrolls == 1 ? "Rolled the dice, 1 roll left." :
                            remainingrolls == 0 ? "Rolled the dice, no rolls left." :
-                           "Rolled the dice, " + remainingrolls + " rolls left.";
+                           "Rolled the dice, " + remainingrolls + " rolls left."
 
     for (let i = 0; i < 6; i++) {
         if (!locked[i]) {
-            die[i].value = Math.ceil(Math.random()*6);
-            die[i].innerHTML = die[i].value;
-            dievalues[i] = die[i].value;
+            die[i].value = Math.ceil(Math.random()*6)
+            die[i].innerHTML = die[i].value
+            dievalues[i] = die[i].value
         }
     }
 }
@@ -48,25 +48,20 @@ function rolldice() {
 
 /* Lock die */
 function lock(index) {
-    if (locked[index]) {
-        locked[index] = false;
-        die[index].classList.remove("locked");
-    } else {
-        locked[index] = true;
-        die[index].classList.add("locked");
-    }
+    locked[index] = locked[index] ? false : true
+    die[index].classList.toggle("locked")
 }
 
 
 /* Unlocking die */
 function unlock() {
     for (let i = 0; i < locked.length; i++) {
-        die[i].innerHTML = 0;
-        dievalues[i] = 0;
+        die[i].innerHTML = 0
+        dievalues[i] = 0
 
         if (locked[i]) {
-            locked[i] = false;
-            die[i].classList.remove("locked");
+            locked[i] = false
+            die[i].classList.remove("locked")
         }
     }
 }
@@ -76,11 +71,11 @@ function unlock() {
 function checkFinished() {
     for (let i = 0; i < finished.length; i++) {
         if (finished[i] == 6 || (i > 0 && finished[i] == 3)) {
-            categories[i].disabled = true;
-            categories[i].style.background = "#4b5320";
-            categories[i].style.color = "#010103";
-            finished[i] = 10;
-            collapse(i);
+            categories[i].disabled = true
+            categories[i].style.background = "#4b5320"
+            categories[i].style.color = "#010103"
+            finished[i] = 10
+            collapse(i)
         }
     }
 }
@@ -88,34 +83,34 @@ function checkFinished() {
 
 /* Disables buttons */
 function disable(e) {
-    category[e].disabled = true;
-    category[e].style.background = "#4b5320";
-    category[e].style.color = "#010103";
+    category[e].disabled = true
+    category[e].style.background = "#4b5320"
+    category[e].style.color = "#010103"
 
-    rolled = 0;
-    remainingrolls += 3;
-    dierollmsg.innerHTML = "Check! " + remainingrolls + " rolls left.";
+    rolled = 0
+    remainingrolls += 3
+    dierollmsg.innerHTML = "Check! " + remainingrolls + " rolls left."
 
-    window.scroll({ top: 0, behavior: "smooth" });
-    checkFinished();
-    unlock();
+    window.scroll({ top: 0, behavior: "smooth" })
+    checkFinished()
+    unlock()
 }
 
 
 /* Opens and closes categories */
 function collapse(index) {
-    categories[index].classList.toggle("active");
-    const content = categories[index].nextElementSibling;
+    categories[index].classList.toggle("active")
+    const content = categories[index].nextElementSibling
 
     if (content.style.maxHeight) {
-        content.style.maxHeight = null;
+        content.style.maxHeight = null
     } else {
-        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.maxHeight = content.scrollHeight + "px"
     }
 
     for (let i = 0; i < categories.length; i++) {
         if (i != index) {
-            categories[i].nextElementSibling.style.maxHeight = null;
+            categories[i].nextElementSibling.style.maxHeight = null
         }
     }
 }
@@ -123,23 +118,23 @@ function collapse(index) {
 
 /* Opens modal and shows stats */
 function gameover() {
-    modal.style.display = "block";
-    totalsum += bonus >= 84 ? 50 : 0;
-    totaltime = Math.round(performance.now() / 1000);
+    modal.style.display = "block"
+    totalsum += bonus >= 84 ? 50 : 0
+    totaltime = Math.round(performance.now() / 1000)
 
-    document.querySelector("#modal-score").innerHTML = "You got a total score of " + totalsum + ".";
-    document.querySelector("#modal-time").innerHTML = calculateTime(totaltime);
-    
-    document.querySelector("#score").value = totalsum;
-    document.querySelector("#time").value = totaltime;
+    document.querySelector("#modal-score").innerHTML = "You got a total score of " + totalsum + "."
+    document.querySelector("#modal-time").innerHTML = calculateTime(totaltime)
+
+    document.querySelector("#score").value = totalsum
+    document.querySelector("#time").value = totaltime
 }
 
 /* Helper function to calculate game time */
 function calculateTime(seconds) {
-    let text = "You spent ";
+    let text = "You spent "
 
     if (seconds > 60) {
-        const minutes = Math.round(seconds / 60);
+        const minutes = Math.round(seconds / 60)
         if (minutes > 1) {
             text += minutes + " minutes and " + seconds % 60 + " seconds playing."
         } else {
@@ -149,5 +144,5 @@ function calculateTime(seconds) {
         text += seconds + " seconds playing."
     }
 
-    return text;
+    return text
 }
