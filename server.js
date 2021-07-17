f5c3c2544926060b3bdf71726c9e1df8a3ef00a8
Sergey <thefanthomasfan@gmail.com>
2021-07-17 22:36:01 +0200

    Removed reduntant dependencies

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 /* Dependencies and middleware */
 const express = require('express')
 const session = require('express-session')
-const { request } = require('express')
 const flash = require('express-flash')
 const passport = require('passport')
 const bcrypt = require('bcrypt')
-const path = require('path')
 const PORT = 3130
 
 const app = express()
